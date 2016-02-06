b419d1c9318d48cd0977fd69de2b433b5461e71a
CagatayOgut <ogut@dbis.rwth-aachen.de>
2016-02-06 08:27:27 -0800

    typo

--- eis/entrypoint.sh
+++ eis/entrypoint.sh
@@ -3,12 +3,12 @@
 cd EIS
 # do necessary replacements by environment variable values
 echo "setting config ..."
-sed -i "s#EIS_DB_NAME#${EIS_DB_NAME}#g" /etc/system.ers.properties
-sed -i "s#EIS_DB_USER#${EIS_DB_USER}#g" /etc/system.ers.properties
-sed -i "s#EIS_DB_PASS#${EIS_DB_PASS}#g" /etc/system.ers.properties
+sed -i "s#EIS_DB_NAME#${EIS_DB_NAME}#g" etc/system.ers.properties
+sed -i "s#EIS_DB_USER#${EIS_DB_USER}#g" etc/system.ers.properties
+sed -i "s#EIS_DB_PASS#${EIS_DB_PASS}#g" etc/system.ers.properties
 
 echo "conf printout ...."
-cat /etc/system.ers.properties
+cat etc/system.ers.properties
 
 chmod u+x bin/start_network.sh
 sleep 0.01 # just for avoid 'text file busy' error
